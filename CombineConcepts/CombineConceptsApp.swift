ac3e0ce2371f321b76fdbbf343fa82a70544bdba
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-09-16 21:24:37 +0300

    Initial Commit

---
+++ CombineConcepts/CombineConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineConceptsApp.swift
+//  CombineConcepts
+//
+//  Created by Yasin Erdemli on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
