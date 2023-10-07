f67bafa4a87931c46b36e3d528f892a9d8d53767
yasin <yasinerdemli35@icloud.com>
2023-10-07 21:00:03 +0300

    add passthroughSubject

---
+++ CombineConcepts/PassthroughSubject/PassthroughSubjectIntro.swift
@@ -0,0 +1,18 @@
+//
+//  PassthroughSubjectIntro.swift
+//  CombineConcepts
+//
+//  Created by Yasin Erdemli on 07.10.2023.
+//
+
+import SwiftUI
+
+struct PassthroughSubjectIntro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PassthroughSubjectIntro()
+}
