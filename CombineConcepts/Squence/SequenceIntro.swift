ee1a965ad88cb8747ee77f4db509b88b2c881697
yasin <yasinerdemli35@icloud.com>
2023-10-07 21:35:03 +0300

    add sequence

---
+++ CombineConcepts/Squence/SequenceIntro.swift
@@ -0,0 +1,18 @@
+//
+//  SequenceIntro.swift
+//  CombineConcepts
+//
+//  Created by Yasin Erdemli on 07.10.2023.
+//
+
+import SwiftUI
+
+struct SequenceIntro: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SequenceIntro()
+}
---
+++ CombineConcepts/Squence/SequenceWithString.swift
@@ -0,0 +1,18 @@
+//
+//  SequenceWithString.swift
+//  CombineConcepts
+//
+//  Created by Yasin Erdemli on 07.10.2023.
+//
+
+import SwiftUI
+
+struct SequenceWithString: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SequenceWithString()
+}
